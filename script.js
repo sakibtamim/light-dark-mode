b6af949ab1cf44b0d0fe82310a66a754d6da391c
sakibtamim <you@example.com>
2024-09-08 23:24:22 +0600

    2nd commit

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-// const toggleSwitch = document.querySelector('input[type="checkbox"]');
-
-// // switch theme dynamically
-// function switchTheme(event){
-//     console.log(event);
-// }
-
-// // event listenser
-// toggleSwitch.addEventListener('change', switchTheme);
-
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 
+// switch theme dynamically
+function switchTheme(event){
+    console.log(event.target.checked);
+}
 
-toggleSwitch.addEventListener('change', function () {
-    console.log(this.checked); // true if checked, false if not
-});
+// event listenser
+toggleSwitch.addEventListener('change', switchTheme);
